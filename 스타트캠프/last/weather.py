d22b99813a2c0e3f772bbab560a84252bc90393e
Shinyoung Park <sin5015243@naver.com>
2021-07-16 21:44:05 +0900

    last

--- 스타트캠프/last/weather.py
+++ 스타트캠프/last/weather.py
@@ -8,4 +8,3 @@ weather_state_name=response['consolidated_weather'][2]['weather_state_name']
 applicable_date=response['consolidated_weather'][2]['applicable_date']
 print(weather_state_name)
 print(applicable_date)
-
